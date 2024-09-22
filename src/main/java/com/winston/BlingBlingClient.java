5c75d28c47b42a9cfaa041ebba0a43c253a0f5cb
wildbery <103512355+wildbery@users.noreply.github.com>
2024-09-22 19:28:01 +0900

    Initial commit

---
+++ src/main/java/com/winston/BlingBlingClient.java
@@ -0,0 +1,10 @@
+package com.winston;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlingBlingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/winston/BlingBlingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.winston;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlingBlingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
